05b5553104fae9b6121d9a4f7bfe36b3387608bc
DilanLivera <DilanLivera@users.noreply.github.com>
2019-07-10 16:22:54 +1000

    add middlewear for errors and update server.js file

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`App is listening at port ${PORT}`));
+app.use(express.static('public'));
+
+//home route
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+//middlewear for errors
+app.use((req, res, next) => {
+  const error = new Error("Page Not Found!!!");
+  error.status(404);
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500)
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+const listener = app.listen(PORT, () => console.log(`App is listening on port ${listener.address().port}`));
